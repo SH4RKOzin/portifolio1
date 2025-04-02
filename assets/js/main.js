20ae0dff651aec77e456e8560069c1ef39cef79c
SH4RKOzin <144727906+SH4RKOzin@users.noreply.github.com>
2025-04-02 16:21:13 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Designer Gráfico","","Desenvolvedor Web"],
+  strings : ["Designer Gráfico","","Programador Web"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
